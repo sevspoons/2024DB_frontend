e012fd5e38776858555d7e00eca0736e524e33e1
sevspoons <2996362334@qq.com>
2024-10-27 17:09:24 +0800

    信息添加表单

--- src/views/overview/component/dishInfoForm.vue
+++ src/views/overview/component/dishInfoForm.vue
@@ -4,7 +4,7 @@
     <el-form ref="formRef" :model="formModel" label-width="80px">
       <el-row>
         <el-col :span="10">
-          <el-form-item label="菜名" :rules="notEmptyRule">
+          <el-form-item label="菜名" prop="name" :rules="notEmptyRule">
             <el-input
               v-model="formModel.name"
               clearable
@@ -15,7 +15,7 @@
           </el-form-item>
         </el-col>
         <el-col :span="8" :offset="2">
-          <el-form-item label="菜系" :rules="notEmptyRule">
+          <el-form-item label="菜系" prop="type" :rules="notEmptyRule">
             <el-input
               v-model="formModel.type"
               clearable
@@ -26,20 +26,20 @@
         </el-col>
       </el-row>
       <el-row>
-        <el-form-item label="位置" :rules="notEmptyRule">
+        <el-form-item label="位置" prop="canteen" :rules="notEmptyRule">
           <el-input v-model="formModel.canteen" clearable />
         </el-form-item>
       </el-row>
       <el-row>
-        <el-form-item label="价格" :rules="notEmptyRule">
+        <el-form-item label="价格" prop="price" :rules="notEmptyRule">
           <el-input v-model="formModel.price" clearable type="number">
             <template #append>元</template>
           </el-input>
         </el-form-item>
-        <el-form-item label="评分" :rules="notEmptyRule">
+        <el-form-item label="评分" prop="rate" :rules="notEmptyRule">
           <el-rate v-model="formModel.rate" allow-half />
         </el-form-item>
-        <el-form-item label="菜品分量" :rules="notEmptyRule">
+        <el-form-item label="菜品分量" prop="quantity" :rules="notEmptyRule">
           <el-rate
             v-model="formModel.quantity"
             allow-half
@@ -49,22 +49,27 @@
           />
         </el-form-item>
       </el-row>
-      <el-form-item label="简介" :rules="notEmptyRule">
-        <el-input
-          v-model="formModel.content"
-          clearable
-          maxlength="60"
-          show-word-limit
-          type="textarea"
-          :autosize="{ minRows: 12, maxRows: 14 }"
-        />
-      </el-form-item>
+      <el-col :span="22">
+        <el-form-item label="简介" :rules="notEmptyRule">
+          <el-input
+            v-model="formModel.content"
+            clearable
+            maxlength="60"
+            show-word-limit
+            type="textarea"
+            :autosize="{ minRows: 10, maxRows: 12 }"
+          />
+        </el-form-item>
+      </el-col>
     </el-form>
     <el-row>
-      <el-col :span="4" :offset="10">
-        <el-button @click="handelClick(formModel)">{{
+      <el-col :span="4" :offset="8">
+        <el-button @click="handelSubmit(formRef)">{{
           props.btnText
         }}</el-button>
+      </el-col>
+      <el-col :span="4" :offset="2">
+        <el-button @click="handelClear(formRef)">清空</el-button>
       </el-col>
     </el-row>
   </el-dialog>
@@ -72,8 +77,11 @@
 
 <script setup lang="ts">
 import { ref, reactive, toRaw, onMounted } from "vue";
+import type { FormInstance } from "element-plus";
 import type { dishInfo } from "@/api/common";
 import { Chicken } from "@element-plus/icons-vue";
+import { message } from "@/utils/message";
+const formRef = ref<FormInstance>();
 const icons = [Chicken, Chicken, Chicken];
 const colors = ["#f85f73", "#00adb5", "#ff9a00"];
 const props = defineProps({
@@ -88,11 +96,28 @@ const notEmptyRule = [{ required: true, message: "不能为空", trigger: "blur"
 onMounted(() => {
   formModel.value = props.getInit();
 });
+const handelSubmit = (formEl: FormInstance | undefined) => {
+  if (!formEl) return;
+  formEl.validate(valid => {
+    if (valid) {
+      props.handelClick(formModel.value);
+      dialogEnable.value = false;
+      handelClear(formEl);
+    } else {
+      message("缺少必填项!", { type: "error" });
+    }
+  });
+};
+const handelClear = (formEl: FormInstance | undefined) => {
+  if (!formEl) return;
+  formEl.resetFields();
+  formModel.value = props.getInit();
+};
 
+//expose func
 const open = () => {
   dialogEnable.value = true;
 };
-
 defineExpose({
   open
 });
