0ca5e5623eadc99c64b617a711b3a344a2919d52
首墨 李 <2459469072@qq.com>
2024-11-05 08:41:49 +0800

    debug :bug:

---
+++ src/views/userInfo/component/mealRecords.vue
@@ -0,0 +1,26 @@
+<template>
+  <el-scrollbar height="400px">
+    <p v-for="item in HistoryList" :key="item" class="scrollbar-demo-item">
+      {{ item }}
+    </p>
+  </el-scrollbar>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+const HistoryList = ref(["炒饭", "炒面", "炒饼", "抄棵"]);
+</script>
+
+<style scoped>
+.scrollbar-demo-item {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 50px;
+  margin: 10px;
+  text-align: center;
+  border-radius: 4px;
+  background: var(--el-color-primary-light-9);
+  color: var(--el-color-primary);
+}
+</style>
