68ae6ca79e1d11cd084cc4b5495ef02234ba1a1a
sevspoons <2996362334@qq.com>
2024-11-25 19:35:16 +0800

    总表页bug修复

--- src/views/overview/api.ts
+++ src/views/overview/api.ts
@@ -3,7 +3,7 @@ import { getOverviewByPage } from "@/api/common";
 export const getOverview = _config => {
   const data = {
     config: {
-      canteen: _config.canteen,
+      canteen: _config.canteen == null ? "" : _config.canteen,
       maxPrice: _config.maxPrice == null ? -1 : _config.maxPrice
     }
   };
